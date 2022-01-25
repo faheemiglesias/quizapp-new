70d055caba71d4326b343ba37115411b2dc97713
Faheem <faheem.m@cubettech.com>
2022-01-25 11:09:11 +0530

    result route added in index

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/quiz.routes")(app);
 require("./app/routes/question.routes")(app);
 require("./app/routes/submission.routes")(app);
+require("./app/routes/result.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
